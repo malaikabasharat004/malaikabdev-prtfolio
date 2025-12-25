088817775abde6a7870e3e31b4007a74be7c0e35
malaikabasharat004 <malaikabdev@gmail.com>
2025-12-25 05:08:54 +0500

    blog stuff removed from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
       <RevealFx translateY="16" delay={0.6}>
         <Projects range={[1, 1]} />
       </RevealFx>
-      {routes["/blog"] && (
+       {/* {routes["/blog"] && (
         <Column fillWidth gap="24" marginBottom="l">
           <Row fillWidth paddingRight="64">
             <Line maxWidth={48} />
@@ -115,14 +115,14 @@ export default function Home() {
               </Heading>
             </Row>
             <Row flex={3} paddingX="20">
-              <Posts range={[1, 2]} columns="2" />
+             <Posts range={[1, 2]} columns="2" />
             </Row>
           </Row>
           <Row fillWidth paddingLeft="64" horizontal="end">
             <Line maxWidth={48} />
           </Row>
         </Column>
-      )}
+      )} */}
       <Projects range={[2]} />
       <Mailchimp />
     </Column>
